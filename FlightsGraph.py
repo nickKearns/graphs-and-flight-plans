5cf941396e0be5a837d97124fa83100c771bcf4c
nickKearns <nickkearns133@gmail.com>
2020-07-16 15:15:21 -0400

    adding classes

---
+++ FlightsGraph.py
@@ -0,0 +1,118 @@
+from collections import deque
+
+class AirportVertex(object):
+    """
+    Defines a single vertex and its neighbors.
+    """
+
+    def __init__(self, vertex_id):
+        """
+        Initialize a vertex and its neighbors dictionary.
+        
+        Parameters:
+        vertex_id (string): A unique identifier to identify this vertex.
+        """
+        self.__id = vertex_id
+        self.__neighbors_dict = {} # id -> object
+
+    def add_neighbor(self, vertex_obj):
+        """
+        Add a neighbor by storing it in the neighbors dictionary.
+
+        Parameters:
+        vertex_obj (Vertex): An instance of Vertex to be stored as a neighbor.
+        """
+        self.__neighbors_dict[vertex_obj.__id] = vertex_obj
+
+    def __str__(self):
+        """Output the list of neighbors of this vertex."""
+        neighbor_ids = list(self.__neighbors_dict.keys())
+        return f'{self.__id} adjacent to {neighbor_ids}'
+
+    def __repr__(self):
+        """Output the list of neighbors of this vertex."""
+        return self.__str__()
+
+    def get_neighbors(self):
+        """Return the neighbors of this vertex."""
+        return list(self.__neighbors_dict.values())
+
+    def get_id(self):
+        """Return the id of this vertex."""
+        return self.__id
+
+
+
+
+
+class Graph:
+    """ Graph Class
+    Represents a directed or undirected graph.
+    """
+    def __init__(self, is_directed=True):
+        """
+        Initialize a graph object with an empty vertex dictionary.
+
+        Parameters:
+        is_directed (boolean): Whether the graph is directed (edges go in only one direction).
+        """
+        self.__vertex_dict = {} # id -> object
+        self.__is_directed = is_directed
+
+    def add_vertex(self, vertex_id):
+        """
+        Add a new vertex object to the graph with the given key and return the vertex.
+        
+        Parameters:
+        vertex_id (string): The unique identifier for the new vertex.
+
+        Returns:
+        Vertex: The new vertex object.
+        """
+        new_vertex = AirportVertex(vertex_id)
+        self.__vertex_dict[vertex_id] = new_vertex
+        return new_vertex
+        
+
+    def get_vertex(self, vertex_id):
+        """Return the vertex if it exists."""
+        if vertex_id not in self.__vertex_dict:
+            return None
+
+        vertex_obj = self.__vertex_dict[vertex_id]
+        return vertex_obj
+
+    def add_edge(self, vertex_id1, vertex_id2):
+        """
+        Add an edge from vertex with id `vertex_id1` to vertex with id `vertex_id2`.
+
+        Parameters:
+        vertex_id1 (string): The unique identifier of the first vertex.
+        vertex_id2 (string): The unique identifier of the second vertex.
+        """
+        vertex1 = self.get_vertex(vertex_id1)
+        vertex2 = self.get_vertex(vertex_id2)
+        vertex1.add_neighbor(vertex2)
+        if self.__is_directed == False:
+            vertex2.add_neighbor(vertex1)
+        
+        
+    def get_vertices(self):
+        """
+        Return all vertices in the graph.
+        
+        Returns:
+        List<Vertex>: The vertex objects contained in the graph.
+        """
+        return list(self.__vertex_dict.values())
+
+    def contains_id(self, vertex_id):
+        return vertex_id in self.__vertex_dict
+
+    def __str__(self):
+        """Return a string representation of the graph."""
+        return f'Graph with vertices: {self.get_vertices()}'
+
+    def __repr__(self):
+        """Return a string representation of the graph."""
+        return self.__str__()
