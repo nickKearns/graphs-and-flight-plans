2fd09d3a8425df9a192269a21101139ce4c4935e
nickKearns <nickkearns133@gmail.com>
2020-07-18 19:20:21 -0400

    working on scc func

--- FlightsGraph.py
+++ FlightsGraph.py
@@ -1,10 +1,8 @@
 from collections import deque
 
-class AirportVertex(object):
-    """
-    Defines a single vertex and its neighbors.
-    """
 
+class AirportVertex(object):
+    
     def __init__(self, vertex_id):
         """
         Initialize a vertex and its neighbors dictionary.
@@ -12,52 +10,61 @@ class AirportVertex(object):
         Parameters:
         vertex_id (string): A unique identifier to identify this vertex.
         """
-        self.__id = vertex_id
-        self.__neighbors_dict = {} # id -> object
+        self.id = vertex_id
+        self.neighbors_dict = {} # id -> (obj, weight)
 
-    def add_neighbor(self, vertex_obj):
+    def add_neighbor(self, vertex_obj, weight):
         """
         Add a neighbor by storing it in the neighbors dictionary.
-
         Parameters:
         vertex_obj (Vertex): An instance of Vertex to be stored as a neighbor.
+        weight (number): The weight of this edge.
         """
-        self.__neighbors_dict[vertex_obj.__id] = vertex_obj
+        if vertex_obj.get_id() in self.neighbors_dict.keys():
+            return # it's already a neighbor
 
-    def __str__(self):
-        """Output the list of neighbors of this vertex."""
-        neighbor_ids = list(self.__neighbors_dict.keys())
-        return f'{self.__id} adjacent to {neighbor_ids}'
-
-    def __repr__(self):
-        """Output the list of neighbors of this vertex."""
-        return self.__str__()
+        self.neighbors_dict[vertex_obj.get_id()] = (vertex_obj, weight)
 
     def get_neighbors(self):
         """Return the neighbors of this vertex."""
-        return list(self.__neighbors_dict.values())
+        return [neighbor for (neighbor, weight) in self.neighbors_dict.values()]
+
+    def get_neighbors_with_weights(self):
+        """Return the neighbors of this vertex."""
+        return list(self.neighbors_dict.values())
 
     def get_id(self):
         """Return the id of this vertex."""
-        return self.__id
+        return self.id
+
+    def __str__(self):
+        """Output the list of neighbors of this vertex."""
+        # neighbor_ids = [neighbor.get_id() for neighbor in self.get_neighbors()]
+        # return f'{self.id} adjacent to {neighbor_ids}'
+        return self.get_id()
+
+    def __repr__(self):
+        """Output the list of neighbors of this vertex."""
+        neighbor_ids = [neighbor.get_id() for neighbor in self.get_neighbors()]
+        return f'{self.id} adjacent to {neighbor_ids}'
 
 
 
 
 
-class Graph:
-    """ Graph Class
-    Represents a directed or undirected graph.
-    """
+
+class FlightsGraph(object):
+
+    INFINITY = float('inf')
+
     def __init__(self, is_directed=True):
         """
         Initialize a graph object with an empty vertex dictionary.
-
         Parameters:
         is_directed (boolean): Whether the graph is directed (edges go in only one direction).
         """
-        self.__vertex_dict = {} # id -> object
-        self.__is_directed = is_directed
+        self.vertex_dict = {}
+        self.is_directed = is_directed
 
     def add_vertex(self, vertex_id):
         """
@@ -65,54 +72,266 @@ class Graph:
         
         Parameters:
         vertex_id (string): The unique identifier for the new vertex.
-
         Returns:
         Vertex: The new vertex object.
         """
-        new_vertex = AirportVertex(vertex_id)
-        self.__vertex_dict[vertex_id] = new_vertex
-        return new_vertex
-        
+        if vertex_id in self.vertex_dict.keys():
+            return False # it's already there
+        vertex_obj = AirportVertex(vertex_id)
+        self.vertex_dict[vertex_id] = vertex_obj
+        return True
 
     def get_vertex(self, vertex_id):
         """Return the vertex if it exists."""
-        if vertex_id not in self.__vertex_dict:
+        if vertex_id not in self.vertex_dict.keys():
             return None
-
-        vertex_obj = self.__vertex_dict[vertex_id]
+        vertex_obj = self.vertex_dict[vertex_id]
         return vertex_obj
-
-    def add_edge(self, vertex_id1, vertex_id2):
+    
+    def add_edge(self, vertex_id1, vertex_id2, weight):
         """
         Add an edge from vertex with id `vertex_id1` to vertex with id `vertex_id2`.
-
         Parameters:
         vertex_id1 (string): The unique identifier of the first vertex.
         vertex_id2 (string): The unique identifier of the second vertex.
+        weight (number): The edge weight.
         """
-        vertex1 = self.get_vertex(vertex_id1)
-        vertex2 = self.get_vertex(vertex_id2)
-        vertex1.add_neighbor(vertex2)
-        if self.__is_directed == False:
-            vertex2.add_neighbor(vertex1)
-        
-        
+        all_ids = self.vertex_dict.keys()
+        if vertex_id1 not in all_ids or vertex_id2 not in all_ids:
+            return False
+        vertex_obj1 = self.get_vertex(vertex_id1)
+        vertex_obj2 = self.get_vertex(vertex_id2)
+        vertex_obj1.add_neighbor(vertex_obj2, weight)
+        if not self.is_directed:
+            vertex_obj2.add_neighbor(vertex_obj1, weight)
+
     def get_vertices(self):
+        """Return all the vertices in the graph"""
+        return list(self.vertex_dict.values())
+
+    def __iter__(self):
+        """Iterate over the vertex objects in the graph, to use sytax:
+        for vertex in graph"""
+        return iter(self.vertex_dict.values())
+
+    def union(self, parent_map, vertex_id1, vertex_id2):
+        """Combine vertex_id1 and vertex_id2 into the same group."""
+        vertex1_root = self.find(parent_map, vertex_id1)
+        vertex2_root = self.find(parent_map, vertex_id2)
+        parent_map[vertex1_root] = vertex2_root
+
+
+    def find(self, parent_map, vertex_id):
+        """Get the root (or, group label) for vertex_id."""
+        if(parent_map[vertex_id] == vertex_id):
+            return vertex_id
+        return self.find(parent_map, parent_map[vertex_id])
+
+
+
+
+
+    def minimum_spanning_tree_kruskal(self):
         """
-        Return all vertices in the graph.
+        Use Kruskal's Algorithm to return a list of edges, as tuples of 
+        (start_id, dest_id, weight) in the graph's minimum spanning tree.
+        """
+        # TODO: Create a list of all edges in the graph, sort them by weight 
+        # from smallest to largest
         
-        Returns:
-        List<Vertex>: The vertex objects contained in the graph.
+        vertices_checked_for_weights = set()
+        weights = []
+
+
+        for vertex_id in self.vertex_dict:
+            vertex_obj = self.get_vertex(vertex_id)
+            for neighbor, weight in vertex_obj.get_neighbors_with_weights():
+                neighbor_id = neighbor.get_id()
+                weights.append((weight, vertex_id, neighbor_id))
+                    
+            vertices_checked_for_weights.add(neighbor)
+        weights = sorted(weights)
+        # return weights
+
+
+        # TODO: Create a dictionary `parent_map` to map vertex -> its "parent". 
+        # Initialize it so that each vertex is its own parent.
+
+        parent_map = {}
+        for vertex_id in self.vertex_dict:
+            parent_map[vertex_id] = vertex_id
+        
+
+
+        # TODO: Create an empty list to hold the solution (i.e. all edges in the 
+        # final spanning tree)
+
+        solution_list = []
+
+        # TODO: While the spanning tree holds < V-1 edges, get the smallest 
+        # edge. If the two vertices connected by the edge are in different sets 
+        # (i.e. calling `find()` gets two different roots), then it will not 
+        # create a cycle, so add it to the solution set and call `union()` on 
+        # the two vertices.
+
+
+        while len(solution_list) < len(self.vertex_dict) - 1:
+            current_smallest_weight, vertex_one_id, vertex_two_id = weights.pop(0)
+            if self.find(parent_map, vertex_one_id) != self.find(parent_map, vertex_two_id):
+                mst_edge = (vertex_one_id, vertex_two_id, current_smallest_weight)
+                solution_list.append(mst_edge)
+                self.union(parent_map, vertex_one_id, vertex_two_id)
+
+        return solution_list
+
+
+
+
+    def find_shortest_path(self, start_id, target_id):
         """
-        return list(self.__vertex_dict.values())
+        Use Dijkstra's Algorithm to return the total weight of the shortest path
+        from a start vertex to a destination.
+        """
+        # TODO: Create a dictionary `vertex_to_distance` and initialize all
+        # vertices to INFINITY - hint: use `float('inf')`
 
-    def contains_id(self, vertex_id):
-        return vertex_id in self.__vertex_dict
 
-    def __str__(self):
-        """Return a string representation of the graph."""
-        return f'Graph with vertices: {self.get_vertices()}'
+        vertex_to_distance = {}
 
-    def __repr__(self):
-        """Return a string representation of the graph."""
-        return self.__str__()
+        for vertex in self.vertex_dict:
+            if vertex == start_id:
+                vertex_to_distance[vertex] = 0
+            else:
+                vertex_to_distance[vertex] = float('inf')
+
+
+        # TODO: While `vertex_to_distance` is not empty:
+        # 1. Get the minimum-distance remaining vertex, remove it from the
+        #    dictionary. If it is the target vertex, return its distance.
+        # 2. Update that vertex's neighbors by adding the edge weight to the
+        #    vertex's distance, if it is lower than previous.
+
+        while vertex_to_distance:
+
+            current_shortest_edge = float('inf')
+            current_vertex_id = None
+            #find the current shortest distance from the start vertex
+            for vertex_id in vertex_to_distance:
+                weight = vertex_to_distance[vertex_id]
+                if weight < current_shortest_edge:
+                    current_lowest = weight
+                    current_vertex_id = vertex_id
+
+            if current_vertex_id == target_id:
+                return vertex_to_distance[current_vertex_id]
+
+            
+            current_vertex_obj = self.get_vertex(current_vertex_id)
+
+
+            for neighbor, weight in current_vertex_obj.get_neighbors_with_weights():
+                neighbor_id = neighbor.get_id()
+                if neighbor_id in vertex_to_distance:
+                    vertex_to_distance[neighbor_id] = min(weight + vertex_to_distance[current_vertex_id], vertex_to_distance[neighbor_id])
+
+            # print(vertex_to_distance)
+
+            vertex_to_distance.__delitem__(current_vertex_id)
+        
+    def floyd_warshall(self):
+        """
+        Return the All-Pairs-Shortest-Paths dictionary, containing the shortest
+        paths from each vertex to each other vertex.
+        """
+
+        dist = dict()
+
+        all_vertex_ids = self.vertex_dict.keys()
+
+        for vertex1 in all_vertex_ids:
+            dist[vertex1] = dict()
+            for vertex2 in all_vertex_ids:
+                dist[vertex1][vertex2] = FlightsGraph.INFINITY
+            dist[vertex1][vertex1] = 0
+
+
+        all_vertex_objs = self.get_vertices()
+
+        for vertex in all_vertex_objs:
+            neighbors_with_weights = vertex.get_neighbors_with_weights()
+
+            for neighbor, weight in neighbors_with_weights:
+                dist[vertex.get_id()][neighbor.get_id()] = weight
+
+
+
+
+        for k in self.vertex_dict.keys():
+            for i in self.vertex_dict.keys():
+                for j in self.vertex_dict.keys():
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+        return dist
+
+
+    def fillOrder(self,vertex,visited, stack): 
+        visited.add(vertex)
+
+        for neighbor in vertex.get_neighbors(): 
+            if neighbor not in visited: 
+                self.fillOrder(neighbor, visited, stack) 
+        stack = stack.append(vertex) 
+
+
+    def transpose_graph(self):
+        '''
+        This function will reverse the direction of the edges in a graph
+        to find the transpose of the graph
+        '''
+
+        graph_copy = self
+
+        for vertex1 in self.get_vertices():
+            for vertex2 in vertex1.get_neighbors():
+                graph_copy.add_edge(vertex2.get_id(), vertex1.get_id(), 0)
+
+        return graph_copy
+
+
+
+    def dfs_traversal_recursive(self, vertex, visited_vertices):
+        scc_group = []
+        scc_group.append(vertex)
+        visited_vertices.add(vertex)
+        
+        for neighbor in vertex.get_neighbors():
+            if neighbor not in visited_vertices:
+                self.dfs_traversal_recursive(neighbor, visited_vertices)
+        return scc_group
+
+    def kosaraju_find_SCC(self):
+        '''
+        Use kosaraju's algorithm to find the strongly connected components
+        in a directed graph. This algorithm does not take into account the weights
+        because the weights do not change whether a group of vertices are strongly connected or 
+        not
+        '''
+
+        stack = deque()
+
+        visited_vertices = set()
+
+        for vertex in self.get_vertices():
+            if vertex not in visited_vertices:
+                self.fillOrder(vertex, visited_vertices, stack)
+
+        transposed_graph = self.transpose_graph()
+
+        visited_vertices.clear()
+
+        while stack:
+            last_vertex = stack.pop()
+            current_scc_group = []
+            if last_vertex not in visited_vertices:
+                current_scc_group = transposed_graph.dfs_traversal_recursive(last_vertex, visited_vertices)
+        print('')
+        
---
+++ main.py
@@ -0,0 +1,58 @@
+from FlightsGraph import AirportVertex, FlightsGraph
+
+if __name__ == "__main__":
+    
+    # airport_test = AirportVertex('Test')
+    airport_BOS = AirportVertex('BOS')
+    airport_JFK = AirportVertex('JFK')
+    airport_CLT = AirportVertex('CLT')
+    airport_ORD = AirportVertex('ORD')
+    airport_ATL = AirportVertex('ATL')
+    airport_DFW = AirportVertex('DFW')
+    airport_DEN = AirportVertex('DEN')
+    airport_LAS = AirportVertex('LAS')
+    airport_LAX = AirportVertex('LAX')
+    airport_SFO = AirportVertex('SFO')
+    airport_SEA = AirportVertex('SEA')
+
+    graph = FlightsGraph(False)
+
+    graph.add_vertex(airport_BOS.get_id())
+    graph.add_vertex(airport_JFK.get_id())
+    graph.add_vertex(airport_CLT.get_id())
+    graph.add_vertex(airport_ORD.get_id())
+    graph.add_vertex(airport_ATL.get_id())
+    graph.add_vertex(airport_DFW.get_id())
+    graph.add_vertex(airport_DEN.get_id())
+    graph.add_vertex(airport_LAS.get_id())
+    graph.add_vertex(airport_LAX.get_id())
+    graph.add_vertex(airport_SFO.get_id())
+    graph.add_vertex(airport_SEA.get_id())
+
+
+
+    graph.add_edge(airport_SEA.get_id(), airport_SFO.get_id(), 5)
+    # graph.add_edge(airport_SFO.get_id(), airport_SEA.get_id(), 5)
+    graph.add_edge(airport_SFO.get_id(), airport_LAX.get_id(), 5)
+    # graph.add_edge(airport_LAX.get_id(), airport_SFO.get_id(), 5)
+    graph.add_edge(airport_LAX.get_id(), airport_LAS.get_id(), 5)
+    # graph.add_edge(airport_LAS.get_id(), airport_LAX.get_id(), 5)
+    graph.add_edge(airport_SFO.get_id(), airport_LAS.get_id(), 8)
+    graph.add_edge(airport_LAS.get_id(), airport_DEN.get_id(), 2)
+    graph.add_edge(airport_DEN.get_id(), airport_ORD.get_id(), 5)
+    graph.add_edge(airport_ORD.get_id(), airport_JFK.get_id(), 10)
+    graph.add_edge(airport_JFK.get_id(), airport_BOS.get_id(), 2)
+    graph.add_edge(airport_JFK.get_id(), airport_CLT.get_id(), 3)
+    graph.add_edge(airport_DFW.get_id(), airport_ORD.get_id(), 15)
+    graph.add_edge(airport_DFW.get_id(), airport_ATL.get_id(), 10)
+    graph.add_edge(airport_ATL.get_id(), airport_JFK.get_id(), 30)
+    # graph.add_edge(airport_BOS.get_id(), airport_JFK.get_id(), 2)
+    graph.add_edge(airport_BOS.get_id(), airport_ATL.get_id(), 15)
+    graph.add_edge(airport_BOS.get_id(), airport_SFO.get_id(), 50)
+
+    print(graph.find_shortest_path('SFO', 'BOS'))
+    print(graph.minimum_spanning_tree_kruskal())
+    print("      ")
+    print(graph.floyd_warshall())
+    graph.kosaraju_find_SCC()
+    
